ee70503bd20abee0e6aca0924e3d12bad27f94de
slo331 <slo331@yahoo.com>
2021-03-18 18:51:48 +0800

    Added new sections of the template

--- src/_modules/molecules/image-widget/image-widget.js
+++ src/_modules/molecules/image-widget/image-widget.js
@@ -2,7 +2,35 @@
 
 export default class ImageWidget {
   constructor() {
-    this.name = 'ImageWidget';
-    console.log('ImageWidget');
+    let $imgWidget = $('.image-widget');
+    this.$modal = $('.modal');
+
+    $imgWidget.map((i, el) => {
+      let $el = $(el);
+      let $imgBtn = $('.btn--plus', $el);
+      let $caption = $('.image-caption', $el);
+      let $title = $('.title', $caption);
+      let $desc = $('.desc', $caption);
+
+      $imgBtn.on('click', e => {
+        e.preventDefault();
+        this._showModal($title, $desc);
+      })
+    })
   }
+
+  _showModal($title, $desc) {
+		let $init = () => {
+      $('body').toggleClass('fixed');
+      this.$modal.toggleClass('modal--visible');
+      this.$modal.find('.modal-head').empty();
+      this.$modal.find('.modal-body').empty();
+    };
+
+    $.when($init())
+    .done(() => {
+      $title.clone().appendTo(this.$modal.find('.modal-head'));
+      $desc.clone().appendTo(this.$modal.find('.modal-body'));
+    });
+	}
 }
--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -1,11 +1,20 @@
 // Main javascript entry point
 // Should handle bootstrapping/starting application
 'use strict';
+
 import Carousel from '../_modules/molecules/carousel/carousel';
+import ImageWidget from '../_modules/molecules/image-widget/image-widget';
+import Modal from '../_modules/molecules/modal/modal';
 
 $(() => {
   if($('.carousel').length) {
     new Carousel();
   }
+
+  if($('.image-widget').length) {
+    new ImageWidget();
+  }
+
+  new Modal();
 });
 
