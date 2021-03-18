a05d0ea627ec5aa7b1b630635ae820c5874fb9e0
DESKTOP-8OR0HIQ\slo33 <slo331@yahoo.com>
2021-03-18 20:58:38 +0800

    Added footer module in the template

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -2,11 +2,22 @@
 // Should handle bootstrapping/starting application
 'use strict';
 
+import 'slick-carousel';
+import enquire from 'enquire.js';
+
 import Carousel from '../_modules/molecules/carousel/carousel';
 import ImageWidget from '../_modules/molecules/image-widget/image-widget';
 import Modal from '../_modules/molecules/modal/modal';
 
 $(() => {
+  let config = {
+		breakpoints: {
+			tablet: 768,
+			desktop: 1024,
+			lgDesktop: 1280
+		}
+  };
+
   if($('.carousel').length) {
     new Carousel();
   }
@@ -16,5 +27,39 @@ $(() => {
   }
 
   new Modal();
+
+  if($('.topics-wrap').length) {
+    // Mobile & Tablet
+    enquire.register("screen and (max-width: " + (config.breakpoints.desktop - 1) + "px)", {
+      match: () => {
+        $('.topics-list').slick({
+          infinite: true,
+          dots: true,
+          arrows: false,
+          speed: 500,
+          pauseOnHover: true,
+          mobileFirst: true,
+          fade: false,
+          adaptiveHeight: true,
+          accessibility: false,
+          centerMode: true,
+          responsive: [
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            }
+          ]
+        });
+      },
+      unmatch: () => {
+        $('.topics-list').slick('unslick');
+      }
+    });
+
+    
+  }
 });
 
